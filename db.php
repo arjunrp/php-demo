8381a39c0668d405a906a490e41f210d0bc31add
Arjun RP <arj.arjun@gmail.com>
2015-02-25 18:40:56 +0530

    Adding database class

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+class Database(){
+	private $dbname="atm";
+	private $username="root";
+	private $password="root";
+	private $host="localhost";
+	private $db=false;
+
+	public connect(){
+		$this->db = new mysqli($this->host,$this->username,$this->password,$this->dbname);
+		if($this->db->connect_errno){
+			return false;
+		}
+		$this->db::set_charset('utf-8');
+		return $db;
+	}
+}
