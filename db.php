b24010485de1a95a0a7cc7ab2c05e1fc3c44a5c5
Arjun RP <arj.arjun@gmail.com>
2015-02-25 18:48:14 +0530

    Removing mistakes

--- db.php
+++ db.php
@@ -1,17 +1,17 @@
 <?php
-class Database(){
+class Database{
 	private $dbname="atm";
 	private $username="root";
 	private $password="root";
 	private $host="localhost";
 	private $db=false;
 
-	public connect(){
+	public function connect(){
 		$this->db = new mysqli($this->host,$this->username,$this->password,$this->dbname);
 		if($this->db->connect_errno){
 			return false;
 		}
-		$this->db::set_charset('utf-8');
+		$this->db->set_charset('utf-8');
 		return $db;
 	}
 }
